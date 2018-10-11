bd3712d3a7d0c2bce9687ceb9815ec1da892f8ea
Jacek Olszewski <jacek@bee.local>
2018-10-11 19:54:23 +0200

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var http = require('http');
 
 const PORT = 7000;
 const HOST = 'localhost';
-const TIMEOUT = 5000;
+const TIMEOUT = 0;
 
 var app = express();
 app.use(morgan('combined'));
@@ -16,8 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var server = http.createServer(app);
 
-function sleep(time) {
+function sleep(time) { 
     return new Promise((resolve, reject) => {
+        if (time < 0) {
+            reject('INVALID');
+        }
         setTimeout(function() {
             resolve();
       }, time);
@@ -29,7 +32,10 @@ function handle(req, res)  {
     console.log('Headers: ', req.headers);
     console.log('Cookies: ', req.cookies);
     sleep(TIMEOUT).then(()=> {
-        res.send('_OK_');    
+        res.cookie('kookie' , 'value', {});
+        res.send('OK');    
+    }).catch((reason)=> {
+        res.send(reason);   
     })
 }
 
@@ -44,5 +50,5 @@ app.post('/', function (req, res) {
 
 server.listen(PORT, HOST);
 server.on('listening', function() {
-      console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+      console.log('Express server started at http://%s:%s/', server.address().address, server.address().port);
 }); 
